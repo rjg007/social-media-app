04fecca12bbac495988cb373986e39d15a513288
Rahul <rahul@codewave.com>
2023-02-18 01:43:20 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
